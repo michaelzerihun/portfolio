81d0c7028d629451356bd2b87ee4a8f6a56c2d19
michaelzt <michael.zerihun@etmsoftwareplc.com>
2025-02-18 11:06:37 +0300

    quick fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,39 @@
-import Container from '@/components/Container';
-import HomeDescripton from '@/components/HomeDescripton';
-import Photo from '@/components/Photo';
-import SocialLinks from '@/components/SocialLinks';
-import Statistics from '@/components/Statistics';
-import { Button } from '@/components/ui/button';
-import { Download } from 'lucide-react';
+import Container from "@/components/Container";
+import HomeDescripton from "@/components/HomeDescripton";
+import Photo from "@/components/Photo";
+import SocialLinks from "@/components/SocialLinks";
+import Statistics from "@/components/Statistics";
+import { Download } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <Container className='py-10 grid grid-cols-1 md:grid-cols-2 gap-10'>
-        <div className='flex flex-col items-center md:items-start gap-5 md:gap-7 text-center md:text-start'>
+      <Container className="py-10 grid grid-cols-1 md:grid-cols-2 gap-10">
+        <div className="flex flex-col items-center md:items-start gap-5 md:gap-7 text-center md:text-start">
           <div>
-            <h3 className='font-semibold text-white/70 tracking-wider mb-1'>
+            <h3 className="font-semibold text-white/70 tracking-wider mb-1">
               Software Engineer
             </h3>
-            <h2 className='text-3xl md:text-5xl mb-2'>Hello I&apos;m</h2>
-            <h1 className='text-lightSky text-5xl md:text-7xl tracking-normal'>
+            <h2 className="text-3xl md:text-5xl mb-2">Hello I&apos;m</h2>
+            <h1 className="text-lightSky text-5xl md:text-7xl tracking-normal">
               Michael Zerihun
             </h1>
           </div>
-          <div className='w-full h-[170px] md:h-[140px] relative'>
-            <div className='absolute top-0 left-0 w-full h-full'>
+          <div className="w-full h-[170px] md:h-[140px] relative">
+            <div className="absolute top-0 left-0 w-full h-full">
               <HomeDescripton />
             </div>
           </div>
-          <Button className='bg-transparent rounded-full border border-lightSky/50 text-lightSky hover:bg-hoverColor hover:text-black hoverEffect'>
+
+          <Link
+            href="/resume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="h-10 px-4 py-2 bg-transparent rounded-full border border-lightSky/50 text-lightSky hover:bg-hoverColor hover:text-black hoverEffect inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+          >
             Download cv <Download />
-          </Button>
+          </Link>
           <SocialLinks />
           <Statistics />
         </div>
