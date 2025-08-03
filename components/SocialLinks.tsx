f8bfa924c7149d39beb2282f99be39c61b28f78f
michaelz <michaelz>
2025-08-03 23:02:36 +0300

    fix(social media links): github fixed

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,46 +1,46 @@
-import { Facebook, Github, Instagram, Linkedin } from 'lucide-react';
-import Link from 'next/link';
-import React from 'react';
-import { Icon } from '@iconify/react/dist/iconify.js';
+import { Facebook, Github, Instagram, Linkedin } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 const socialData = [
   {
-    title: 'Telegram',
-    icon: <Icon icon='mingcute:telegram-line' width={20} height={20} />,
-    link: 'https://t.me/mxchaelz',
+    title: "Telegram",
+    icon: <Icon icon="mingcute:telegram-line" width={20} height={20} />,
+    link: "https://t.me/mxchaelz",
   },
   {
-    title: 'Github',
+    title: "Github",
     icon: <Github width={20} height={20} />,
-    link: 'https://github.com/MichaelZ1999',
+    link: "https://github.com/michaelzerihun",
   },
   {
-    title: 'Linkedin',
+    title: "Linkedin",
     icon: <Linkedin width={20} height={20} />,
-    link: 'https://www.linkedin.com/in/michael-zerihun-391b94232/',
+    link: "https://www.linkedin.com/in/michael-zerihun-391b94232/",
   },
   {
-    title: 'Facebook',
+    title: "Facebook",
     icon: <Facebook width={20} height={20} />,
-    link: '',
+    link: "",
   },
   {
-    title: 'instagram',
+    title: "instagram",
     icon: <Instagram width={20} height={20} />,
-    link: 'https://www.instagram.com/mxchael.z/',
+    link: "https://www.instagram.com/mxchael.z/",
   },
 ];
 
 const SocialLinks = () => {
   return (
-    <div className='flex items-center gap-3'>
+    <div className="flex items-center gap-3">
       {socialData?.map((item) => (
         <div
           key={item?.title}
-          className='text-lightSky/80 border border-lightSky/30 p-2.5 rounded-full hover:bg-lightSky/10 hover:border-lightSky hover:text-lightSky hoverEffect'
+          className="text-lightSky/80 border border-lightSky/30 p-2.5 rounded-full hover:bg-lightSky/10 hover:border-lightSky hover:text-lightSky hoverEffect"
         >
-          <Link href={item?.link} target='blank'>
-            <span className=''>{item?.icon}</span>
+          <Link href={item?.link} target="blank">
+            <span className="">{item?.icon}</span>
           </Link>
         </div>
       ))}
